9f090ff31158ebef6aa52f530d8c6f78ae462660
LENDRA SYAPUTRA <l3noneone@gmail.com>
2025-05-13 09:38:27 +0700

    Delete web/vite.config.js

--- web/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  preview: {
-    port: 4173,
-    host: true,
-    allowedHosts: ['lia-web-production.up.railway.app'],
-  },
-});
